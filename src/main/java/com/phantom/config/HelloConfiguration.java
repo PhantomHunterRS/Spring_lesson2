7140978545230c931fbc15942b9bc886e14974dc
filyakov.a <ASerfilyakov@mail.ru>
2021-11-29 17:49:35 +0300

    work3_v3

--- src/main/java/com/phantom/config/HelloConfiguration.java
+++ src/main/java/com/phantom/config/HelloConfiguration.java
@@ -3,9 +3,12 @@ package com.phantom.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.phantom")
+
+@EnableWebMvc
 public class HelloConfiguration {
 
 }
--- src/main/java/com/phantom/config/WebConfig.java
+++ src/main/java/com/phantom/config/WebConfig.java
@@ -1,18 +1,35 @@
 package com.phantom.config;
+import com.phantom.HelloSpring;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.Dispatcher;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 @Slf4j
 public class WebConfig implements WebApplicationInitializer{
+
+    public static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         log.info("WebConfig - onStartUp");
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(HelloConfiguration.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DISPATCHER_SERVLET_NAME,dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
 
     }
 }
